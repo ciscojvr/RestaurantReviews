c6d58f5e208c9c510dd3a9a6d3ced91a6e18b7c6
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-31 03:26:33 -0400

    Provided implementation to the search method in YelpClient.

--- RestaurantReviews/YelpClient.swift
+++ RestaurantReviews/YelpClient.swift
@@ -24,5 +24,15 @@ class YelpClient: APIClient {
     // since search is not a generic method, we can finally specify concrete types for Result in our completion handler below.
     func search(withTerm term: String, at coordinate: Coordinate, categories: [YelpCategory] = [], radius: Int? = nil, limit: Int = 50, sortBy sortType: Yelp.YelpSortType = .rating, completion: @escaping (Result<[YelpBusiness], APIError>) -> Void) {
         
+        let endpoint = Yelp.search(term: term, coordinate: coordinate, radius: radius, categories: categories, limit: limit, sortBy: sortType)
+        
+        let request = endpoint.requestWithAuthorizationHeader(oauthToken: token)
+        
+        fetch(with: request, parse: { json -> [YelpBusiness] in
+            guard let businesses = json["businesses"] as? [[String: Any]] else { return [] }
+            
+            return businesses.compactMap { YelpBusiness(json: $0) }
+            
+        }, completion: completion)
     }
 }
