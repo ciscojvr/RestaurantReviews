4106720300184104059ffc67a0ddd87d3bce0745
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-31 18:03:20 -0400

    Completed implementation of YelpBusinessDetailsOperation.

--- RestaurantReviews/YelpBusinessDetailsOperation.swift
+++ RestaurantReviews/YelpBusinessDetailsOperation.swift
@@ -19,6 +19,10 @@ class YelpBusinessDetailsOperation: Operation {
         super.init()
     }
     
+    override var isAsynchronous: Bool {
+        return true
+    }
+    
     private var _finished = false // this is called a backing property since it serves as the underlying storage for a public property. _finished is an Objective-C convention, were private backing instance variables were prefized with an underscore to disambiguate between the backing variable and the public one.
     
     override private(set) var isFinished: Bool { // private(set) makes the setter private
@@ -27,7 +31,24 @@ class YelpBusinessDetailsOperation: Operation {
         }
         
         set {
+            willChangeValue(forKey: "isFinished")
            _finished = newValue // newValue is whatever new value we set to isFinished
+            didChangeValue(forKey: "isFinished")
+            // before and after the value of isFinished changes, we'll notifiy any observers and they can call the getter accordingly.
+        }
+    }
+    
+    private var _executing = false
+    
+    override private(set) var isExecuting: Bool {
+        get {
+            return _executing
+        }
+        
+        set {
+            willChangeValue(forKey: "isExecuting")
+            _executing = newValue
+            didChangeValue(forKey: "isExecuting")
         }
     }
     
@@ -35,6 +56,20 @@ class YelpBusinessDetailsOperation: Operation {
         if isCancelled {
             isFinished = true
             return
+        }
+        
+        isExecuting = true
+        
+        client.updateWithHoursAndPhotos(self.business) { [unowned self] result in // the client is owned by the operation, and we're going to reference self inside the closure. So we need to be aware of memory implications in here. We know that the operation queue we add this operation to is going to hold onto the operation until it is complete, which means, the client is going to exist as well, so we will capture self as an unowned reference.
+            switch result {
+            case .success(_): // we're updathing the business model so we don't want to extract it out of the associated value and do anything with it so we'll discard that.
+                self.isExecuting = false
+                self.isFinished = true
+            case .failure(let error):
+                print(error)
+                self.isExecuting = false
+                self.isFinished = true
+            }
         }
     }
 }
